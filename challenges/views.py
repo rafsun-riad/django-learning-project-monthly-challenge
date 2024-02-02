effc5a450451a0ada18cbe3b158ea07c44ec8dff
rafsun-riad <mruhaquer@gmail.com>
2024-02-02 22:46:10 +0600

    learned for tag

--- challenges/views.py
+++ challenges/views.py
@@ -22,16 +22,15 @@ monthly_challenges_dict = {
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges_dict.keys())
 
-    for month in months:
-        capitalize_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f'<li><a href="{month_path}">{capitalize_month}</a></li>'
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(
+        request,
+        "challenges/index.html",
+        {
+            "months": months,
+        },
+    )
 
 
 def monthly_challenges_by_number(request, month):
